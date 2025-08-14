b80d91731477b2e74f6629af69cab9bd06b8a697
akumImchen <akumimchen1411@gmail.com>
2025-08-14 23:59:55 +0530

    initial commit

--- script.js
+++ script.js
@@ -23,8 +23,70 @@ multiply.addEventListener('click',function(){
 const seven=document.getElementById('sev');
 seven.addEventListener('click',function(){
     const value=this.innerHTML;
-        screen.innerHTML+=value;
-    
-    
-    
+        screen.innerHTML+=value; 
+})
+const eight=document.getElementById('eight');
+eight.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const nine=document.getElementById('nine');
+nine.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const sub=document.getElementById('sub');
+sub.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const four=document.getElementById('four');
+four.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const five=document.getElementById('five');
+five.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const six=document.getElementById('six');
+six.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const equal=document.getElementById('equal');
+equal.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const one=document.getElementById('one');
+one.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const two=document.getElementById('two');
+two.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const three=document.getElementById('three');
+three.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const add=document.getElementById('add');
+add.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const zero=document.getElementById('zero');
+zero.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
+})
+const dot=document.getElementById('dot');
+dot.addEventListener('click',function(){
+    const value=this.innerHTML;
+    screen.innerHTML+=value;
 })
